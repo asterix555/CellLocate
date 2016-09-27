dbf3a71a0285284262f8da9a092b5ef73849c1ac
Erwin Preuner <erwin@preuner.at>
2016-09-27 07:12:30 +0200

    initial commit

---
+++ firmware/CellLocate.cpp
@@ -0,0 +1,131 @@
+#include "application.h"
+#include "CellLocate.h"
+#include "cellular_hal.h"
+
+// Requires local compile
+// ALL_LEVEL, TRACE_LEVEL, DEBUG_LEVEL, INFO_LEVEL, WARN_LEVEL, ERROR_LEVEL, PANIC_LEVEL, NO_LOG_LEVEL
+//SerialDebugOutput debugOutput(9600, ALL_LEVEL);
+void CellLocate::timeout_set(uint32_t timeout_ms) {
+  cellTimeout = timeout_ms;
+  cellTimeStart = millis();
+}
+
+bool CellLocate::is_timeout() {
+  return (cellTimeout && ((millis()-cellTimeStart) > cellTimeout));
+}
+
+void CellLocate::timeout_clear() {
+  cellTimeout = 0;
+}
+
+bool CellLocate::is_matched() {
+  return CellLocate::ok;
+}
+/* Cell Locate Callback */
+int CellLocate::_cbLOCATE(int type, const char* buf, int len)
+{
+  if (type == TYPE_PLUS) {
+    // DEBUG CODE TO SEE EACH LINE PARSED
+    // char line[256];
+    // strncpy(line, buf, len);
+    // line[len] = '\0';
+    // Serial.printf("LINE: %s",line);
+
+    // <response_type> = 1:
+    //+UULOC: <date>,<time>,<lat>,<long>,<alt>,<uncertainty>,<speed>,<direction>,
+    //        <vertical_acc>,<sensor_used>,<SV_used>,<antenna_status>,<jamming_status>
+    //+UULOC: 25/09/2013,10:13:29.000,45.7140971,13.7409172,266,17,0,0,18,1,6,3,9
+    int count = 0;
+    //
+    // TODO: %f was not working for float on LAT/LONG, so opted for capturing strings for now
+    if ( (count = sscanf(buf, "\r\n+UULOC: %d/%d/%d,%d:%d:%d.%*d,%[^,],%[^,],%d,%d,%d,%d,%d,%d,%d,%d,%d\r\n",
+    CellLocate::day,
+    CellLocate::month,
+    CellLocate::year,
+    CellLocate::hour,
+    CellLocate::minute,
+    CellLocate::second,
+    CellLocate::lat,
+    CellLocate::lng,
+    CellLocate::altitude,
+    CellLocate::uncertainty,
+    CellLocate::speed,
+    CellLocate::direction,
+    CellLocate::vertical_acc,
+    CellLocate::sensor_used,
+    CellLocate::sv_used,
+    CellLocate::antenna_status,
+    CellLocate::jamming_status) ) > 0 ) {
+      // UULOC Matched
+      CellLocate::count = count;
+      CellLocate::ok = true;
+    }
+  }
+  return WAIT;
+}
+
+int CellLocate::cell_locate(uint32_t timeout_ms) {
+  CellLocate::count = 0;
+  CellLocate::ok = false;
+  if (RESP_OK == Cellular.command(5000, "AT+ULOCCELL=0\r\n")) {
+    CellLocate *_self;
+      if (RESP_OK == Cellular.command(CellLocate::_cbLOCATE, &CellLocate::_self, timeout_ms, "AT+ULOC=2,2,1,%d,5000\r\n", timeout_ms/1000)) {
+      timeout_set(timeout_ms);
+      if (CellLocate::count > 0) {
+        return CellLocate::count;
+      }
+      return 0;
+    }
+    else {
+      return -2;
+      // Serial.println("Error! No Response from AT+LOC");
+    }
+  }
+  // else Serial.println("Error! No Response from AT+ULOCCELL");
+  return -1;
+}
+
+bool CellLocate::in_progress() {
+  if (!CellLocate::is_matched() && !CellLocate::is_timeout()) {
+    return true;
+  }
+  else {
+    CellLocate::timeout_clear();
+    return false;
+  }
+}
+
+bool CellLocate::get_response() {
+  // Send empty string to check for URCs that were slow
+  Cellular.command(CellLocate::_cbLOCATE, &CellLocate::_self, 1000, "");
+  if (CellLocate::count > 0) {
+    return true;
+  }
+  return false;
+}
+
+void CellLocate::display() {
+  /* The whole kit-n-kaboodle */
+  Serial.printlnf("\r\n%d/%d/%d,%d:%d:%d,LAT:%s,LONG:%s,%d,UNCERTAINTY:%d,SPEED:%d,%d,%d,%d,%d,%d,%d,MATCHED_COUNT:%d",
+  CellLocate::month,
+  CellLocate::day,
+  CellLocate::year,
+  CellLocate::hour,
+  CellLocate::minute,
+  CellLocate::second,
+  CellLocate::lat,
+  CellLocate::lng,
+  CellLocate::altitude,
+  CellLocate::uncertainty,
+  CellLocate::speed,
+  CellLocate::direction,
+  CellLocate::vertical_acc,
+  CellLocate::sensor_used,
+  CellLocate::sv_used,
+  CellLocate::antenna_status,
+  CellLocate::jamming_status,
+  CellLocate::count);
+
+  /* A nice map URL */
+  Serial.printlnf("\r\nhttps://www.google.com/maps?q=%s,%s\r\n",CellLocate::lat,CellLocate::lng);
+}
---
+++ firmware/CellLocate.h
@@ -0,0 +1,59 @@
+#include "application.h"
+#include "cellular_hal.h"
+
+// struct MDM_CELL_LOCATE {
+
+class CellLocate
+{
+  CellLocate& _self;
+  private:
+  public:
+
+  int day = 0;
+  int month = 0;
+  int year = 0;
+  int hour = 0;
+  int minute = 0;
+  int second = 0;
+  char lat[14] = "";
+  char lng[14] = "";
+  int altitude = 0; // only for GNSS positioning
+  int uncertainty = 0;
+  int speed = 0; // only for GNSS positioning
+  int direction = 0; // only for GNSS positioning
+  int vertical_acc = 0; // only for GNSS positioning
+  int sensor_used = 0; // 0: the last fix in the internal database, 2: CellLocate(R) location information
+  int sv_used = 0; // only for GNSS positioning
+  int antenna_status = 0; // only for GNSS positioning
+  int jamming_status = 0; // only for GNSS positioning
+  int count = 0;
+  bool ok = 0;
+  int size = 0;
+
+  void MDM_CELL_LOCATE();
+
+  bool displayed_once = false;
+  volatile uint32_t cellTimeout;
+  volatile uint32_t cellTimeStart;
+
+  // Requires local compile
+  // ALL_LEVEL, TRACE_LEVEL, DEBUG_LEVEL, INFO_LEVEL, WARN_LEVEL, ERROR_LEVEL, PANIC_LEVEL, NO_LOG_LEVEL
+  //SerialDebugOutput debugOutput(9600, ALL_LEVEL);
+  void timeout_set(uint32_t timeout_ms);
+
+  bool is_timeout();
+
+  void timeout_clear();
+
+  bool is_matched();
+  /* Cell Locate Callback */
+  int _cbLOCATE(int type, const char* buf, int len);
+
+  int cell_locate(uint32_t timeout_ms);
+
+  bool in_progress();
+
+  bool get_response();
+
+  void display();
+};
